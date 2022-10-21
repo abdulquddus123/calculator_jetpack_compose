85f6d7651df68c3fb9c7758ac30ad254f131769f
areyen <abdul.quddus@sslwireless.com>
2022-10-22 00:38:37 +0600

    issue fixing

--- app/src/main/java/com/example/myapplication/CalculatorViewModel.kt
+++ app/src/main/java/com/example/myapplication/CalculatorViewModel.kt
@@ -31,17 +31,21 @@ class CalculatorViewModel : ViewModel() {
     }
 
     private fun performCalculation() {
-       val number1=state.number1.toDoubleOrNull()
-       val number2=state.number2.toDoubleOrNull()
-        if(number1 !=null && number2!=null){
-            var result=when(state.operation){
-                is CalculatorOperation.Add->number1+number2
-                is CalculatorOperation.Multiply->number1*number2
-                is CalculatorOperation.Substract->number1-number2
-                is CalculatorOperation.Divide->number1/number2
-                null->return
+        val number1 = state.number1.toDoubleOrNull()
+        val number2 = state.number2.toDoubleOrNull()
+        if(number1 != null && number2 != null) {
+            val result = when(state.operation) {
+                is CalculatorOperation.Add -> number1 + number2
+                is CalculatorOperation.Substract -> number1 - number2
+                is CalculatorOperation.Multiply -> number1 * number2
+                is CalculatorOperation.Divide -> number1 / number2
+                null -> return
             }
-            state=state.copy(number1=result.toString().take(15),number2="", operation = null)
+            state = state.copy(
+                number1 = result.toString().take(15),
+                number2 = "",
+                operation = null
+            )
         }
     }
 
@@ -57,25 +61,27 @@ class CalculatorViewModel : ViewModel() {
     }
 
     private fun enterOperation(operation: CalculatorOperation) {
-
-        if (state.number1.isNotBlank()) {
+        if(state.number1.isNotBlank()) {
             state = state.copy(operation = operation)
         }
     }
 
     private fun enterNumber(num: Int) {
-        if (state.operation == null) {
-            if (state.number1.length >= MAX_NUM_LENGTH) {
+        if(state.operation == null) {
+            if(state.number1.length >= MAX_NUM_LENGTH) {
                 return
             }
-            state = state.copy(number1 = state.number1 + num)
-            return
-            if (state.number2.length >= MAX_NUM_LENGTH) {
-                return
-            }
-            state = state.copy(number2 = state.number2 + num)
+            state = state.copy(
+                number1 = state.number1 + num
+            )
             return
         }
+        if(state.number2.length >= MAX_NUM_LENGTH) {
+            return
+        }
+        state = state.copy(
+            number2 = state.number2 + num
+        )
     }
 
     companion object {
